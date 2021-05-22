aed52d12f9ae7eabb41ede1cf942da9fbd2fbdd6
Holytimes-Fr <73057306+Holytimes-Fr@users.noreply.github.com>
2021-05-22 23:31:20 +0200

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" type="text/css" href="../style/bootstrap.css">
+<h1>Administration - Connexion</h1>
+<form action="" method="POST">
+<h3>Pseudo :</h3>	<input type="text" name="username"/><br></br>
+<h3>Mdp :</h3>	<input type="password" name="password"/><br></br>
+<input type="submit" name="submit"/><br></br>
+</form>
