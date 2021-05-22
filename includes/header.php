7a6abd0e05c7766384ecb87fbfc5981ca2090c3f
Holytimes-Fr <Holytimes@outlook.fr>
2021-05-22 13:09:58 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<link href="style/bootstrap.css" type="text/css" rel="stylesheet"/>
-</head>
-<body>
-<h1>E-Commerce</h1>
-</body>
+	<head>
+		<link href="style/bootstrap.css" type="text/css" rel="stylesheet"/>
+	</head>
+	<header>
+		<ul>
+			<li><a href="index.php">Accueil</a></li>
+			<li><a href="index.php">Informatique</a></li>
+			<li><a href="index.php">Jardin</a></li>
+			<li><a href="index.php">Meuble</a></li>
+			<li><a href="index.php">A propos</a></li>
+		</ul>
+	</header>
 </html>
